d5daca7746ba00070f1b62e4838824b6e2673346
Giovanni Roswin <gio93ivan@gmail.com>
2020-11-17 15:53:41 +0700

    testing library

---
+++ app/src/main/java/com/deeeplabs/deeeplabsandroidcode/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.deeeplabs.deeeplabsandroidcode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
